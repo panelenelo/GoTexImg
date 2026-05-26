35fa52a946c3db7915dd4c9c1473bdf9d5898d39
panelenelo <panelenelo@gmail.com>
2026-05-26 11:19:44 -0300

    established connection with db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"GoTexImg/internal/models"
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const port string = ":8181"
@@ -14,22 +18,50 @@ const port string = ":8181"
 type application struct {
 	PageContent PageContent
 	Client      *http.Client
-	DB          models.TeximgModel
+	DB          *models.TeximgModel
 }
 
 func main() {
-	addr := flag.String("addr", port, "HTTP network address")
-	flag.Parse()
+
+	dsn, addr := parseFlags()
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	app := &application{
 		PageContent: PageContent{},
 		Client:      &http.Client{Timeout: 5 * time.Second},
+		DB:          &models.TeximgModel{DB: db},
 	}
 
 	log.Printf("Server starting on localhost%s", port)
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(addr, app.routes())
 	if err != nil {
 		log.Print(err.Error())
 	}
 	os.Exit(1)
+}
+
+func parseFlags() (string, string) {
+	addr := flag.String("addr", port, "HTTP network address")
+
+	dbName := flag.String("dbName", os.Getenv("POSTGRES_DB"), "DB name")
+	dbPort := flag.String("dbPort", os.Getenv("PGRS_PORT"), "DB port")
+	dbHost := flag.String("dbHost", os.Getenv("PGRS_HOST"), "DB host address")
+	dbUser := flag.String("dbUser", os.Getenv("POSTGRES_USER"), "DB User")
+	dbPass := flag.String("dbPass", os.Getenv("POSTGRES_PASSWORD"), "DB password")
+	flag.Parse()
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable connect_timeout=5", *dbHost, *dbPort, *dbUser, *dbPass, *dbName)
+
+	return dsn, *addr
 }
